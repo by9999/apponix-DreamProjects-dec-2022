13cdd54e9a8f4742b3bfe0772aac76a3aaf48c0c
waseem ahmed <waseemslyar@gmail.com>
2023-01-19 09:36:46 +0530

    created top nav bar

--- fullstack/frontend/library/ecommerce/flipkart/src/app/pages/home/Home.tsx
+++ fullstack/frontend/library/ecommerce/flipkart/src/app/pages/home/Home.tsx
@@ -1,29 +1,36 @@
-import { Container } from "@mui/material"
+import { Container, Grid } from "@mui/material"
 import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 import { Multiplewaysofcss } from "../../component/notes/css/Multiplewaysofcss"
+import TopNavBar from "../../component/TopNavBar/TopNavBar"
 
-export const Home:React.FC<{}>=()=>{
-   var [userName,setUserName]=useState('')
-   var userState=useLocation()
-   useEffect(()=>{
+export const Home: React.FC<{}> = () => {
+   var [userName, setUserName] = useState('')
+   var userState = useLocation()
+   useEffect(() => {
       setUserName(userState.state.username)
-   },[])
+   }, [])
    console.log(userState.state.username)
-  
-   var size={
-    extraSmalls:'xm',
-    small:'sm',
-    large:'lg', 
-    extralarge:'xl',
-    medium:'md'
+
+   var size = {
+      extraSmalls: 'xm',
+      small: 'sm',
+      large: 'lg',
+      extralarge: 'xl',
+      medium: 'md'
    }
    return <>
-    
-    <Container maxWidth="xl" >
-      <h1>welcome {userName}!</h1>
 
-    <Multiplewaysofcss/>
-    </Container>
-    </>
-    }
+      <Container maxWidth="xl" >
+         
+         <Grid  xs={12}>
+            <Grid item>
+<TopNavBar/>
+            </Grid>
+
+         </Grid>
+         <h1>welcome {userName}!</h1>
+         <Multiplewaysofcss />
+      </Container>
+   </>
+}
